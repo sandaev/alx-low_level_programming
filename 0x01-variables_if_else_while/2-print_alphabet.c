16be112774ac69b86086f10045a1c501aac8abae
Sanni Ozigi <sandaev20@gmail.com>
2022-09-09 12:18:40 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int counter;
+
+	counter = 97;
+	while (counter <= 122)
+	{
+		putchar(counter);
+		counter += 1;
+	}
+	return (0);
+}
