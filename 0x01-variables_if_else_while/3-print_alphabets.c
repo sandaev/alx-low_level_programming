b57f638fcec87adab2bb3754d9e8a59a803c62bb
Sanni Ozigi <sandaev20@gmail.com>
2022-09-09 12:57:36 -0700

    Lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lowerCount, upperCount;
+
+	lowerCount = 97;
+	upperCount = 65;
+
+	while (lowerCount <= 122)
+	{
+		putchar(lowerCount);
+		lowerCount += 1;
+	}
+	while (upperCount <= 90)
+	{
+		putchar(upperCount);
+		upperCount += 1;
+	}
+	return (0);
+
