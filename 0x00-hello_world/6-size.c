f2ad49b0a1b1feced3f02ce5b9589b9ba8c8d313
Sanni Ozigi <sandaev20@gmail.com>
2022-09-08 21:36:21 -0700

    sizeof type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d%s\n", (unsigned) sizeof(char), " byte(s)");
+	print("Size of an int: %d%s\n", (unsigned) sizeof(int), "byte(s)");
+	printf("Size of a long int: %d%s\n", (unsigned) sizeof(long int), " byte(s)");
+	printf("Size of a long long int: %d%s\n", (unsigned) sizeof(long long int), " byte(s)");
+	printf("Size of a float: %d%s\n", (unsigned) sizeof(float), " byte(s)");
+	return (0);
+}
