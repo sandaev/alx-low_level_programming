94319f54510fbfd7c5a1e2840689caba21e9fffd
Sanni Ozigi <sandaev20@gmail.com>
2022-09-09 20:44:15 -0700

    Skip e and q

---
+++ 0x01-variables_if_else_while/4-print_alphbt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count;
+
+	count = 97;
+
+	while (count <= 122)
+	{
+		if ( count != 101 || count != 113)
+			putchar(count);
+		count += 1;
+	}
+	putchar(10);
+	return (0);
+}
