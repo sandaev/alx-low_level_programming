7806e4a29682d44a8ed7edbb07ab9e4bdffed53b
Sanni Ozigi <sandaev20@gmail.com>
2022-09-09 21:12:26 -0700

    Alpabet reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 122;
+	while (c >= 97)
+	{
+		putchar(c);
+		c -= 1;
+	}
+	putchar(10);
+	return (10);
+}
