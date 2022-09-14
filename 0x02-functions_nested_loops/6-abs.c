fae8892a5d2d2cb15bc03163e7235a1fe02609e2
Sanni Ozigi <sandaev20@gmail.com>
2022-09-14 03:33:56 -0700

    Fix betty

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,16 @@
 /**
  * _abs - Computes the absolute value of an integer
  *
+ * @n - integer whose absolute value is to be returned
  * Return: Value of integer as a positive number.
  */
 int _abs(int n)
 {
 	int result;
-	 if (n >= 0)
-		 result = n;
-	 else if (n < 0)
-		 result = -1 * n;
-	 return (result);
+
+	if (n >= 0)
+	       	result = n;
+	else if (n < 0)
+	       	result = -1 * n;
+	return (result);
 }
