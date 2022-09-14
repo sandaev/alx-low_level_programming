ee86d7e9db570cac49e28994a54909dabf64f51a
Sanni Ozigi <sandaev20@gmail.com>
2022-09-13 19:21:58 -0700

    Print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints lowercase letters 10 times
+ *
+ * Return: No return value
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		print_alphabet();
+		i++;
+	}
+	_putchar('\n');
+}
