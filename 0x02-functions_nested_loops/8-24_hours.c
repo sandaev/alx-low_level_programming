831ec79e6ac22a5d24dfbb151e899373b6829873
Sanni Ozigi <sandaev20@gmail.com>
2022-09-14 07:54:26 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * jack_bauer- Prints every minute of the day of jack bauer
+ *
+ * Return: Returns void
+ */
+void jack_bauer(void)
+{
+	int h1, h2, h3, h4;
+
+	h1 = 0;
+	while (h1 <= 2)
+	{
+		h2 = 0;
+		while (h2 <= 3)
+		{
+			m1 = 0;
+			while (m1 <= 5)
+			{
+				m2 = 0;
+				while (m2 <= 9)
+				{
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(m1);
+					_putchar(m2);
+					_putchar('\n');
+					m2++;
+				}
+				m1++;
+			}
+			h2++;
+		}
+		h1++;
+	}
+}
+
