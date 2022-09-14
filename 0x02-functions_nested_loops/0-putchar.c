bedc97f214042e6cd916bda451592a3c070990cd
Sanni Ozigi <sandaev20@gmail.com>
2022-09-13 18:40:47 -0700

    fix betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,18 @@
 /**
  * main - Entry point
  *
- * Return- Always 0 (Success);
+ * Return- Always 0 (Success)
  */
 int main(void)
 {
 	char myString[] = "_putchar";
 	int i;
 
-	for ( i = 0; myString[i] != '\0'; i++)
-	{	_putchar(myString[i]);
+	for (i = 0; myString[i] != '\0'; i++)
+	{
+		_putchar(myString[i]);
 	}
 	_putchar('\n');
 
 	return (0);
 }
-
