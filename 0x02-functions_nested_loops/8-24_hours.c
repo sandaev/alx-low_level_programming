ff36226fd646550b6cba4147ac958963c87e8349
Sanni Ozigi <sandaev20@gmail.com>
2022-09-14 08:48:10 -0700

    rollback putchar fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,11 +20,11 @@ void jack_bauer(void)
 				m2 = 0;
 				while (m2 <= 9)
 				{
-					_putchar(h1, '0');
-					_putchar(h2, '0');
+					_putchar(h1);
+					_putchar(h2);
 					_putchar(':');
-					_putchar(m1, '0');
-					_putchar(m2, '0');
+					_putchar(m1);
+					_putchar(m2);
 					_putchar('\n');
 					m2++;
 				}
