9edfbf7985189f75c8fc2d445bc8ef5a9c13cc60
Sanni Ozigi <sandaev20@gmail.com>
2022-09-14 03:04:30 -0700

    Absolute value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ *
+ * Return: Value of integer as a positive number.
+ */
+int _abs(int n)
+{
+	int result;
+	 if (n >= 0)
+		 result = num;
+	 else if (num < 0)
+		 result = -1 * num;
+	 return (result);
+}
