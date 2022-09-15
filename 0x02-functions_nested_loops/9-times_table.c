928637ab306ab15b1222fc6d51a10296cab106bc
Sanni Ozigi <sandaev20@gmail.com>
2022-09-14 21:00:38 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,12 @@ void times_table(void)
 {
 	char row, col;
 
-	for (row = 0; row <= 9; row++)
+	for (row = '0'; row <= '9'; row++)
 	{
-		for (col = 0; col <= 9; col++)
+		for (col = '0'; col <= '9'; col++)
 		{
-			_putchar(row);
 			_putchar(row * col);
-			if (col < 9)
+			if (col < '9')
 				_putchar(',');
 			_putchar(' ');
 		}
