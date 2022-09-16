6b529f5e1d1901e80184771db70dc84f868f30dc
Sanni Ozigi <sandaev20@gmail.com>
2022-09-16 08:06:06 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_numbers- Prints the numbers from 0 to 9,
+ * followed by a newline
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
