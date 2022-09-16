2aa6d555e252c233af562710fbdd29d35da1ee38
Sanni Ozigi <sandaev20@gmail.com>
2022-09-16 08:38:23 -0700

    draw line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_line- draws a straightline in the terminal
+ *
+ * @n: determines line length
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if(n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
