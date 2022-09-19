b7278bb6fbcbd10ed308a5fc0c39b2cceca10633
Sanni Ozigi <sandaev20@gmail.com>
2022-09-19 13:25:56 -0700

    Print every other string

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts - prints every other character of a string, followed by a new line
+ *
+ * @s: pointer to the string
+ *
+ * Return: void
+ */
+void puts2 (char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (i % 2)
+		{
+			continue;
+		}
+		_putchar(*(s + i));
+	}
+}
