918b96e7b003cf5fae810e906ef2afeb60f5c968
Sanni Ozigi <sandaev20@gmail.com>
2022-09-19 09:14:03 -0700

    Print strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _puts - prints a string, followed by a newline
+*
+* @str: Pointer to the string to be printed
+*
+* Return: Void
+*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + 1) != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
