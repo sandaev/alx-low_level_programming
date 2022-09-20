fda3ca25f11443ff143a82e0856a340daaa1ea2f
Sanni Ozigi <sandaev20@gmail.com>
2022-09-19 18:54:51 -0700

    Reversed string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * rev_string - Reverses string using pointer
+ *
+ * @s: pointer to the string to be reversed
+ *
+ * Return: Void
+ */
+void rev_string(chat *s)
+{
+	int len, i;
+	char ch, *begin_str, *end_str;
+
+	/* Initializatio */
+	len = 0;
+	*begin_str = s;
+	*end_str = s;
+
+	/* Determine the length of strig */
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		len++
+	}
+	/* Shift end_str to the end */
+	i = 0;
+	while (i < len - 1)
+	{
+		end_str++;
+	}
+	/* Swap characters from beginning and ending */
+	i = 0;
+	while (i < len / 2)
+	{
+		ch = *end_str;
+		*end_str = *begin_str;
+		*begin_str = ch;
+
+		begin_str++;
+		end_str--;
+		i++;
+	}
+}
