ee275ea90a43dd6def240c18f2ef900f7256d9d7
Sanni Ozigi <sandaev20@gmail.com>
2022-09-19 21:24:44 -0700

    Print array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array- prints array of integrers, followed by a new line
+ *
+ * @a: pointer to array of int
+ *
+ * @n: Size of array
+ *
+ * Return : Void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+		{
+			printf("%d, ", *(a + i));
+		}
+		else
+		{
+			printf("%d", *(a + i));
+		}
+	}
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,3 +12,4 @@ void print_rev(char *s);
 void puts2(char *s);
 void rev_string(char *s);
 void puts_half(char * str);
+void print_array(int *a, int n);
