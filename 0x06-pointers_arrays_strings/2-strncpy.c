4dee23524ff09770b43461c81a32875e1ac5a3c6
Sanni Ozigi <sandaev20@gmail.com>
2022-09-22 08:55:11 -0700

    Copy string first commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy - function that xopies a string
+ *
+ * @dest: string to copy to
+ *
+ * @src: string to copy from
+ *
+ * @n: bytes to be copied
+ *
+ * Return: Pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	for ( ; i < n; i++)
+	{
+		*(dest + i) = '\0';
+	}
+	ptrDest = dest;
+	return (ptrDest);
+}
