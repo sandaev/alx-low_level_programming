c596394a819f491cc53ac95bcdbfaba8f3c379c7
Sanni Ozigi <sandaev20@gmail.com>
2022-09-28 08:15:37 -0700

    Indeces recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns power of a number
+ *
+ * @n: base number
+ *
+ * @y: index
+ *
+ * Return: n raiesd to power n
+ */
+int _pow_recursion(int n, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * _pow_recursion(y - 1));
+	}
+}
