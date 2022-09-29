d38ee5445d19909d50ab1142b827a9fa7791095e
Sanni Ozigi <sandaev20@gmail.com>
2022-09-29 09:18:13 -0700

    Find square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _sqrt_recursion - finds the natural sqaure root of numbers
+ *
+ * @n: The number whose square root is to be determined
+ *
+ * Return: 1 if true and 0 if otherwise
+ */
+int _sqrt_recursion(int n)
+{
+	int y = 2;
+
+	return (divide(n, y));
+}
+/**
+ * divide - A helper function that handles the logic
+ *
+ * @n: argument passed to _sqrt_recursion
+ *
+ * @y: divisor
+ *
+ * Return: 1 0r -1
+ */
+
+int divide(int n, int y)
+{
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	if (n < 0 || y >= n)
+	{
+		return (-1);
+	}
+	if (!(n % y))
+	{
+		if (n == y * y)
+		{
+			return (y);
+		}
+		else
+		{
+			divide(n, y + 1);
+		}
+	}
+	else
+	{
+		divide(n, y + 1);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,5 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int n, int y);
+int _sqrt_recursion(int n);
+int divide(int n, int y);
