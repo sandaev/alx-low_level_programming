3f07c35618f7bbcc74cde526a706ed6f9eb857a4
Sanni Ozigi <sandaev20@gmail.com>
2022-09-30 21:15:14 -0700

    Rename file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*main - Entry point
+ *@argc: Number of command line arguments
+ *@argv: array of all command line arguments
+ *Returrn: 0 On success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	/* For loop to print name of programme */
+	for (i == 0; i != '\n'; i++)
+	{
+		_putchar(argv[0][i]);
+	}
+	_putchar('\n');
+}
