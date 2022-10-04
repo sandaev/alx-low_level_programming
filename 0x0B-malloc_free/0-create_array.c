e388000463d758ec2e9b771b4289480f63bb1c0d
Sanni Ozigi <sandaev20@gmail.com>
2022-10-04 05:01:20 -0700

    0-create_array first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of characters
+ *
+ * @size: size of array
+ *
+ * @c: character to be initilized with
+ *
+ * Return: pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	/* Declare variables */
+	unsigned int i;
+	char *ar;
+
+	/* Allocate memory for array */
+	ar = malloc(size * sizeof(char));
+	/* If size is 0 or cannot allocate memory, return 1 */
+	if (size == NULL || ar == NULL)
+	{
+		return (1);
+	}
+	/* Initialize array with c */
+	for (i = 0; i < size; i++)
+	{
+		ar[i] = c;
+	}
+	/* Add null byte to array */
+	ar[i] = '\0';
+	return (ar);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,5 @@
+/**
+ * Description- Header file that contians function prorotypes
+ */
+char *create_array(unsigned int size, char c);
+void _putchar(char c);
