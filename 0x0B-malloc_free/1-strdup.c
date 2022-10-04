45a9e3db977a64b9b7f70fa756e39709cf2c66c2
Sanni Ozigi <sandaev20@gmail.com>
2022-10-04 12:21:28 -0700

    Add stdlib.h header

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 /**
  * _strdup - returns a pointer to a newly allocated space
  * in memory, which contains a copy of the string given as a oarameter
