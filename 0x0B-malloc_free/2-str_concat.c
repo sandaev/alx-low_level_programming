eb667596b3c3a6bd5263966b3ca906bf4c319cc3
Sanni Ozigi <sandaev20@gmail.com>
2022-10-05 08:17:49 -0700

    Fix NULL argument output

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,6 +31,14 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	for (i = 0; s1[i]; i++)
 	{
 		str[i] = s1[i];
