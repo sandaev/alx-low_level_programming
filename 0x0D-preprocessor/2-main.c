5eff8c6cff08050b1afb66a312cc405d1e0483fe
Sanni Ozigi <sandaev20@gmail.com>
2022-10-10 13:30:23 -0700

    First commit for 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Prints the name of the source file using macros
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	/* Declares variables */
+	int i;
+	char s[];
+
+	/* Initialize s with the macors __FILE__ */
+	s = __FILE__;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\0');
+	return (0);
+}
