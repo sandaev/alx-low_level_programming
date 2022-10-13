242bf887ad352eb5e204a063a41e4570e8e3e805
Sanni Ozigi <sandaev20@gmail.com>
2022-10-13 07:35:05 -0700

    Add comment

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,4 +2,5 @@
 int _puchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, unsigned size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif /* #ifndef */
