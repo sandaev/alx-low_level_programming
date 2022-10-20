d1f770cdc444cf7ba17d44b2025359fac29aee18
Sanni Ozigi <sandaev20@gmail.com>
2022-10-20 11:15:52 -0700

    Added header file

---
+++ 0x12-singly_linked_lists/list.h
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#ifndef MAINH
+#define MAINH
+/**
+ * struct list_s - singly linked list
+ * @str: string - (maoolc'ed string)
+ * @len: length of the string
+ * @next: Pointer to next node
+ *
+ * Description - singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *s;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
+int _putchar(char c);
+#endif /* #ifndef MAINH */
