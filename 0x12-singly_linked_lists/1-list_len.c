a4ae0bfd183eb16e1588ae2ffe05240ce3145dae
Sanni Ozigi <sandaev20@gmail.com>
2022-10-20 11:57:41 -0700

    list_len task initial commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * list_len - Returns the number of elements in a linked
+ * list_t list
+ * @h: Pointer to linked list
+ * Return: Number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	/* Variable for holding number of ele */
+	size_t len;
+
+	len = 0;
+	while (h != NULL)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
