2f4f22e1700f8fe2efb9dbe18c7376e8542c9d2a
Sanni Ozigi <sandaev20@gmail.com>
2022-10-20 11:04:03 -0700

    Print_list initial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "list.h"
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: Pointer to the list
+ * Return: Number of elements in list
+ */
+size_t print_list(const list_t *h)
+{
+	/* Number of elements */
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[%d] (null)\n", h->len);
+		}
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
