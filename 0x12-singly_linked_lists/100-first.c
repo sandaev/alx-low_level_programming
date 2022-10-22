4232c12b031ad51cd0a3618f13516c1497025aac
Sanni Ozigi <sandaev20@gmail.com>
2022-10-21 23:07:40 -0700

    Fix betty doc style error

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,15 @@
 #include "list.h"
 
 /**
- * start_fumc - Executes before main executes
+ * start_func - Executes before main executes
+ *
+ * Description: This function prints a string before the main
+ * executes.
  *
  * Return: Nothing
  */
 void start_func(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house
+			upon my back!\n");
 }
