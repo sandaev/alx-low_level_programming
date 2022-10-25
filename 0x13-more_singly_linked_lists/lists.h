f8102fe80c85d5fe32babdec640f4ca7ee6221b8
Sanni Ozigi <sandaev20@gmail.com>
2022-10-24 21:37:44 -0700

    print listin initial commit

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef MAINH
+#define MAINH
+/**
+ * struct listint_s - sindly linked list
+ * @n: Integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+size_t print_listint(const listint_t *h);
+#endif /* #ifndef MAINH */
