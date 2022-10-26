46e8e1e876fbeb1833063cd7d9026563fb5e099b
Sanni Ozigi <sandaev20@gmail.com>
2022-10-26 00:56:13 -0700

    Added brackets to for loop

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	else
 	{
-		for (i = 0; i < (index - 1); i++)
+		for (i = 0; (i < (index - 1)); i++)
 		{
 			tmp = tmp->next;
 		}
