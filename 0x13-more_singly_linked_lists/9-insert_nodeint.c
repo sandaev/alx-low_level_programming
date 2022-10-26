6a1676f39152b6660244968a17bc56d1fe966a2e
Sanni Ozigi <sandaev20@gmail.com>
2022-10-26 00:32:11 -0700

    Fix code for when malloc fials

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,6 +32,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	size = get_size(*head);
 
 	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	new_node->n = n;
 	new_node->next = NULL;
 	if (idx > size)
