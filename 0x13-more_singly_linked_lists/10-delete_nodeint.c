25c0026b8717a2661f2d8274a29f4a0759a00310
Sanni Ozigi <sandaev20@gmail.com>
2022-10-25 22:01:42 -0700

    Fixed error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,11 +23,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		for (i = 0; i < (index - 1); i++)
 		{
 			tmp = tmp->next;
-			tmp->next = tmp->next->next;
-			delete->next = NULL;
-			free(delete);
-			return (1);
 		}
+		delete = tmp->next;
+		tmp->next = tmp->next->next;
+		delete->next = NULL;
+		free(delete);
+		return (1);
 
 	}
 	return (-1);
