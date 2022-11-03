d45154e5c63a4fc4fde1522412f00ed8f9e22e5b
Sanni Ozigi <sandaev20@gmail.com>
2022-11-02 21:54:49 -0700

    Improved code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	if (open(filename, (O_RDWR | O_TRUNC) > 0))
+	if (open(filename, (O_RDWR | O_TRUNC)) > 0)
 	{
 		fd = open(filename, (O_RDWR | O_TRUNC));
 		fw = write(fd, text_content, strlen(text_content));
