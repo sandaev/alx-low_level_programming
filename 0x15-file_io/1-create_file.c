ab518e744e69a49f82250ba65357f682a82114f4
Sanni Ozigi <sandaev20@gmail.com>
2022-11-02 21:47:36 -0700

    Fixed typos and some Wextra flags

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_CREATE | O_RDWR, 0600);
+		fd = open(filename, (O_CREAT | O_RDWR), 0600);
 		if (fd == -1)
 		{
 			return (-1);
@@ -25,15 +25,15 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	if (open(filename, O_RDWR | O_TRUNC > 0))
+	if (open(filename, (O_RDWR | O_TRUNC) > 0))
 	{
-		fd = open(filename, O_RDWR | O_TRUNC);
+		fd = open(filename, (O_RDWR | O_TRUNC));
 		fw = write(fd, text_content, strlen(text_content));
 		fexist = 1;
 	}
 	if (!fexist)
 	{
-		fd = open(filename, O_CREAT | O_RDWR, 0600);
+		fd = open(filename, (O_CREAT | O_RDWR), 0600);
 		if (fd == -1)
 		{
 			return (-1);
