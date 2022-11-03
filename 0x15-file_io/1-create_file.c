fa847d53c0b5e9888a426b5e9e0dc16a720e6be2
Sanni Ozigi <sandaev20@gmail.com>
2022-11-02 21:40:27 -0700

    1-creat_file initial commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * create_file - Creates a file
+ * @filename: pointer to file name
+ * @text_content: pointer to text to
+ * be written in file
+ * Return: ! 0n success, -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fw, fexist;
+
+	fexist = 0;
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREATE | O_RDWR, 0600);
+		if (fd == -1)
+		{
+			return (-1);
+		}
+		close(fd);
+		return (1);
+	}
+	if (open(filename, O_RDWR | O_TRUNC > 0))
+	{
+		fd = open(filename, O_RDWR | O_TRUNC);
+		fw = write(fd, text_content, strlen(text_content));
+		fexist = 1;
+	}
+	if (!fexist)
+	{
+		fd = open(filename, O_CREAT | O_RDWR, 0600);
+		if (fd == -1)
+		{
+			return (-1);
+		}
+		fw = write(fd, text_content, strlen(text_content));
+	}
+	if (fw < 0)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,6 @@
 #define MAINH
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
 #endif /* #ifndef MAINH */
 
