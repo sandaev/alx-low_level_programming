da7419bc57e0df98aace9d748dc1af58c23fe7a6
Sanni Ozigi <sandaev20@gmail.com>
2022-12-09 07:02:27 -0800

    insert update

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts node at a given index
+ * @n: new node
+ * @idx: index
+ * @h: double pointer to link
+ *
+ * Return: address of new node or NULL on failure
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp;
+	dlistint_t *new;
+	int len, c;
+
+	temp = *h;
+	c = 0;
+
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+
+	len = add_dnodeint(h, n);
+
+	if (idx == 0)
+	{
+		new = add_dnodeint_end(h, n);
+	}
+	/* not head not tail */
+	else if (idx > 0 && idx < len)
+	{
+		/* traverse list to the end */
+		while (c < (idx - 1))
+		{
+			++c;
+			temp = temp->next;
+		}
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+		{
+			return (NULL);
+		}
+		new->n = n;
+		new->next = temp->next;
+		temp->next->next->prev = new;
+		temp->next = new;
+		new->prev = temp;
+	}
+
+	return (new);
+}
+
