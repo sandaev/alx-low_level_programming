bd0e57da5bc5dca4974d800792ab1fc3d59eaf19
Sanni Ozigi <sandaev20@gmail.com>
2023-01-06 13:09:22 -0800

    Fix typos

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,6 +12,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
 
-	index = hash_djd2(key) % size;
+	index = hash_djb2(key) % size;
 	return (index);
 }
