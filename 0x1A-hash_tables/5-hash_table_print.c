373071623c449184f6ff9b134887c748c538f6de
Sanni Ozigi <sandaev20@gmail.com>
2023-01-11 11:51:55 -0800

    rename file

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,68 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - This function prints a hash table
+ * @ht: Pointer to hash table
+ *
+ * Return: Nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *head;
+	unsigned long int i;
+	int count = len_ht(ht);
+	int num = 0;
+
+	if (count == 0)
+	{
+		printf("{}\n");
+	}
+	else
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i])
+			{
+				if (ht->array[i]->next)
+				{
+					head = ht->array[i];
+					while (head->next != NULL)
+					{
+						printf("%s: %s, ", head->key, head->value);
+						head = head->next;
+					}
+					num++;
+				}
+				if (num == count - 1)
+				{
+					printf("%s: %s}\n", ht->array[i]->key, ht->array[i]->value);
+				}
+				else
+				{
+					printf("%s: %s, ", ht->array[i]->key, ht->array[i]->value);
+					num++;
+				}
+			}
+		}
+	}
+}
+/**
+ * len_ht - Function that determines the length of table (without collision)
+ * @ht: Pointer to HashTable
+ *
+ * Return: Integer on Success, -1 otherwise
+ */
+int len_ht(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int count = 0;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			count++;
+		}
+	}
+	return (count);
+}
