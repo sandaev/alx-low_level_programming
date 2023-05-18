6f3bb20a2bdcfe939cea89b859414d209b11a92c
sandaev <sandaev20@gmail.com>
2023-05-18 17:16:42 +0000

    Print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints to stderr
+ *
+ * Return: Returns 1
+ */
+int main(void)
+{
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	fputs(s, stderr);
+
+	return (1);
+}
