ab0a0026fe533f74d32117c02304e52e75452749
sandaev <sandaev20@gmail.com>
2023-05-18 16:12:47 +0000

    print string and size of types

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints string to stdout using printf
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
