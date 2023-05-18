94b9ce7ae2c418d63c27041f370fe6725b797d44
sandaev <sandaev20@gmail.com>
2023-05-18 15:39:57 +0000

    Print string to stdout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints a string to stdout
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("Pogramming is like building a multilingual puzzle");
+
+	return (0);
+}
