fd58ed1333efa0d4ceabddb0ca7e64b60c34fba7
sandaev <sandaev20@gmail.com>
2023-05-19 15:24:18 +0000

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print numbers 0 - 9 version2
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
