e6ed2a485299e8ce04795a401986faa5523ff652
sandaev <sandaev20@gmail.com>
2023-05-24 06:58:29 +0000

    Even-valued sum of fibs initial code

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#define maxValue 4000000
+
+/**
+ * main - Finds and prints sum of even-valued fibs < 4 million
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long total, first, second, num;
+
+	total = 0;
+	first = 1;
+	second = 2;
+	num = first + second;
+	while (num <= maxValue)
+	{
+		if (!(num % 2))
+			total += num;
+		first = second;
+		second = num;
+		num = first + second;
+	}
+	printf("%ld\n", total);
+
+	return (0);
+}
