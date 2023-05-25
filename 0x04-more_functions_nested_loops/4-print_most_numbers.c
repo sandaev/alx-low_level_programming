cf92bb56b09abd19afc87dea1baba5617935aa55
sandaev <sandaev20@gmail.com>
2023-05-25 06:19:45 -0700

    tas4 initial code

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers 0 to 9 but not 2
+ * and 4
+ *
+ * Return: Nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '4' || i == '4')
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
