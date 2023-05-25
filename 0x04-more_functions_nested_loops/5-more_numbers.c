ec80143309d7379919e07c35294cc5fd5d8d04d0
sandaev <sandaev20@gmail.com>
2023-05-25 06:59:20 -0700

    Correct logic

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,16 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = '0'; i <= '14'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '14'; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
