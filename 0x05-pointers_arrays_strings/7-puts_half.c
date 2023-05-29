6b6289f77958f91dfeeeb0497cc0e1b41e36feb6
sandaev <sandaev20@gmail.com>
2023-05-29 22:03:43 +0000

    taks7 initial code

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: Pointer to char
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; *(s + len) != '\0'; len++)
+	{
+		;
+	}
+	return (len);
+}
+
+
+/**
+ * puts_half - Prints half of a string
+ * @str: String to print
+ *
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int len, i;
+
+	len = _strlen(str);
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		len = (len - 1) / 2;
+		for (i = len / 2; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
