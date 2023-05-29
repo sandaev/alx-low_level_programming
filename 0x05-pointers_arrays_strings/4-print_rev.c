2e12864c760d4d3660ee4afa2f2ebd5d19de5af7
sandaev <sandaev20@gmail.com>
2023-05-29 09:40:13 +0000

    Fix error

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,20 @@
 #include "main.h"
-
+/**
+ * _strlen - Returns the length of a string
+ * @s: Pointer to char
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int len;
+	
+	for (len = 0; *(s + len) != '\0'; len++)
+	{
+		;
+	}
+	
+	return (len);
+}
 /**
  * print_rev - Prints a string in reverse
  *
