61f04801d509824213208b2d55043b6ccc7ff284
sandaev <sandaev20@gmail.com>
2023-05-29 09:15:45 +0000

    task3 initial code

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string, followed by a new line
+ *
+ * @str: String to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+
+}
