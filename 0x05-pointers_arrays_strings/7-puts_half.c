391b0ff2310a35bdb8ff9ecdfd84d78b1f3fd8cc
sandaev <sandaev20@gmail.com>
2023-05-29 22:27:08 +0000

    Refactor code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -29,21 +29,7 @@ void puts_half(char *str)
 
 	len = _strlen(str);
 
-	/*if (len % 2 == 0)
-	{
-		for (i = len / 2; i < len; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	else
-	{
-		len = (len - 1) / 2;
-		for (i = len / 2; i < len; i++)
-		{
-			_putchar(str[i]);
-		}
-	}*/
+
 	if (len % 2)
 		len = (len - 1) / 2;
 	for (i = len / 2; i < len; i++)
