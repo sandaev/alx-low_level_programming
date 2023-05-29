b540679cab79782c360a47fdf7215804e9cf13dc
sandaev <sandaev20@gmail.com>
2023-05-29 07:45:29 +0000

    task0 initial code

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value Pointed to by a pointer
+ * @n: Pointer variable
+ *
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
