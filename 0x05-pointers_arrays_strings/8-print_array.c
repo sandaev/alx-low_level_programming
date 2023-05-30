06596d3758956d430fd2265c64d98f95c966ed7f
sandaev <sandaev20@gmail.com>
2023-05-30 03:58:09 +0000

    task8 initial code

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Prints array
+ *
+ * @a: Pointer to array of integers
+ * @n: Size of array
+ *
+ * Return: Nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d\n", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
+}
