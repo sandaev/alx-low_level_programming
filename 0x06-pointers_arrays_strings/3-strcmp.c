0fb0b32f88acae6966277fb7a3760bb4417e96ea
sandaev <sandaev20@gmail.com>
2023-05-31 21:17:46 +0000

    Fix error

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,10 @@ int _strcmp(char *s1, char *s2)
 	int i, g, l, e, ret;
 
 	i = g = l = e = 0;
-	while (s1[i] != '\0' || s2[i] != '\0')
+	if (s1[0] == '\0')
+		return (-15);
+
+	while (s1[i] != '\0')
 	{
 		if (s1[i] == s2[i])
 		{
