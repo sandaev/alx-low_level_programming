684c45285f0bed70e4ac42e7c6a596c8a6e4ce93
Sanni <sandae20v@gmail.com>
2023-06-18 08:05:34 +0100

    Root folder

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory
+ * @b: int
+ *
+ * Return: Returns pointer to the allocated memory or NULL
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *men = malloc(b);
+
+	if (mem == 0)
+	{
+		exit(98);
+	}
+
+	return (mem);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+My solution to task on more malloc
