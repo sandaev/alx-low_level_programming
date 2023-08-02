ab30a9c4c721baee037334720edfc726ffd25352
Sanni <sandae20v@gmail.com>
2023-08-02 20:32:47 +0100

    Mandatory tasks initial codes

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - Returns the factorial of a given number
+ *
+ * @n: Given number
+ *
+ * Return: Integer
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
