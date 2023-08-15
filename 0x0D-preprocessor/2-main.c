79346c75a0f50f5542167ac4a055fd96bea40e95
Sanni <sandae20v@gmail.com>
2023-08-15 06:31:43 +0100

    Preprocessor initial codes

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Prints name of file
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* Declaration */
+	int i;
+	char s[] = _FINE_;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
