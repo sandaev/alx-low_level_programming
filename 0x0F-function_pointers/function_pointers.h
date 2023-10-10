8a3a1fb2cc8ab7a543cb397c3750866fc888e440
sandaev <106827899+sandaev@users.noreply.github.com>
2023-10-10 05:49:57 +0100

    Delete 0x0F-function_pointers directory

--- 0x0F-function_pointers/function_pointers.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUCTION_POINTERS_H
-#define FUCTION_POINTERS_H
-
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-
-#endif
