66a7ca5d41e488aeeda8b0b56583d6cb153050b2
sandaev <sandaev20@gmail.com>
2023-10-26 05:41:40 +0100

    fix declaration error

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -33,8 +33,8 @@ int check(char *s, int len, int i)
 {
 	if (s[i] == s[len / 2])
 		return (1);
-	if (s[i	] == s[len - i - 1])
-		return (check(s, len, i));
+	if (s[i] == s[len - i - 1])
+		return (check(s, len, i + 1));
 	return (0);
 }
 /**
