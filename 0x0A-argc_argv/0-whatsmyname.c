a025338f954f8b9f50732c14c07b38f2143643a6
sandaev <sandaev20@gmail.com>
2023-10-27 21:52:48 +0100

    Task0 initial code

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Prints its name
+ *
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+}
