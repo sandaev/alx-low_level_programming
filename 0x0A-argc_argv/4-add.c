30acf99dade209d801e006a357eb5c0f94290380
Sanni <sandae20v@gmail.com>
2023-10-28 11:44:02 +0100

    Fix edge case no integer argument

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,9 +12,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum, i;
+	int sum, i, c, flag;
 
 	sum = 0;
+	c = 0;
+	flag = 0;
 
 	if (argc == 1)
 		printf("%d\n", 0);
@@ -22,15 +24,25 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) == 0 || atoi(argv[i]) < 0)
+			if (atoi(argv[i]) == 0)
 			{
-				printf("Error\n");
-				return (1);
+				c++;
+				flag = 1;
 			}
 			sum += atoi(argv[i]);
 		}
+		if ((c == --i && flag) && sum == 0)
+		{
+			printf("%d\n", 0);
+			return (0);
+		}
+		else if (flag && sum != 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			printf("%d\n", sum);
 	}
-	printf("%d\n", sum);
-
 	return (0);
 }
