ed60ffee03019a7bef21560de4e62a4209c3fcf8
Sanni <sandae20v@gmail.com>
2023-10-31 20:52:29 +0100

    Two D array initial array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - Returns a pointer to a 2D grid
+ *
+ * @w: Width of array
+ * @h: Height of array
+ *
+ * Return: Pointer to the array
+ */
+int **alloc_grid(int w, int h)
+{
+	int **ar, i, j;
+
+	if (w <= 0 || h <= 0)
+		return (NULL);
+	ar = malloc((h * sizeof(int *)));
+	if (ar == NULL)
+	{
+		free(ar);
+		return (NULL);
+	}
+	for (i = 0; i < w; i++)
+	{
+		ar[i] = malloc(w * sizeof(int *));
+	}
+	if (ar == NULL)
+	{
+		free(ar);
+		return (NULL);
+	}
+	for (i = 0; i < h; i++)
+	{
+		for (j = 0; j < w; j++)
+		{
+			ar[i][j] = 0;
+		}
+	}
+	if (ar == NULL)
+	{
+		free(ar);
+		return (NULL);
+	}
+	return (ar);
+}
