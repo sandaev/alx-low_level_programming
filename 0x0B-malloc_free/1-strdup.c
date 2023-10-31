f152d68f72dd793c38729fce462051e4344eb4a4
Sanni <sandae20v@gmail.com>
2023-10-31 06:09:32 +0100

    _strdup initial code

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - Returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string as given
+ *
+ * @str: String to return
+ * Return: Pointer to string
+ */
+char *_strdup(char *str)
+{
+	/* Varaible declaration */
+	if (str == NULL)
+		return (NULL);
+	len = 0;
+	while (str(len))
+		len++;
+	/* Allocate memory */
+	_str = malloc(len * sizeof(char) + 1);
+	if (_str == NULL)
+		return (NULL);
+	/* Copy string into memory */
+	for (i = 0; i <= len; i++)
+		_str[i] = str[i];
+	return (_str);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,4 +3,4 @@
  */
 char *create_array(unsigned int size, char c);
 void _putchar(char c);
-
+char *_strdup(char *str);
