7cc56cf19eaa721cf84c9d76101a2ff533c2f7be
sandaev <sandaev20@gmail.com>
2023-11-02 09:45:29 +0100

    task0 initial code

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: Memory size
+ *
+ * Return: Pointer to memory location
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+
+#endif /* MAIN_H */
