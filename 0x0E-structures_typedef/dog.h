941f16ba479e3d38e6e98cea77c8e6f33c325227
sandaev <sandaev20@gmail.com>
2023-11-06 21:32:41 +0100

    dog initial code

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structures that defines
+ * a dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
