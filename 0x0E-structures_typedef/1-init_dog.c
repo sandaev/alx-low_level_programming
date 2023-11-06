84ae3f310dec8d9126df85312d46d64636dd3637
sandaev <sandaev20@gmail.com>
2023-11-06 22:07:13 +0100

    task one initial code

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializes variable of
+ * type struct dog
+ * @d: struct dog varaiable
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
