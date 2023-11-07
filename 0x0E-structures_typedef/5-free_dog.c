bdafe2aaec543c73e1e58fdd323fe7043cbb4f25
sandaev <sandaev20@gmail.com>
2023-11-07 05:33:52 +0100

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees a dog
+ *
+ * @d: dog object
+ *
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,4 +19,5 @@ void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
 typedef struct dog dog_t;
 dog_t *new_dog(char *, float, char *);
+void free_dog(dog_t *d);
 #endif /* DOG_H */
