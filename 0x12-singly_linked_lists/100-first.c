5a4093d8fd57ea1f8b51dbedd94ddedc443e75ef
Sanni <sandae20v@gmail.com>
2023-11-17 05:32:44 +0100

    Fix typo

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,7 +10,7 @@
  */
 void start_func(void)
 {
-	char *str = "You're beat! and yest, you must allow,";
+	char *str = "You're beat! and yet, you must allow,";
 	char *str2 = "\nI bore my house upon my back!";
 
 	printf("%s%s\n", str, str2);
