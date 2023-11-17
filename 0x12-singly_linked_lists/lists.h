d36d75d035b2133316a24bd2c1d07ce45e97feb2
Sanni <sandae20v@gmail.com>
2023-11-17 05:26:13 +0100

    Task4 initial code

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,5 +25,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void start_func(void) __attribute__((constructor));
+void free_list(list_t *head);
  
 #endif /* LIST_H */
