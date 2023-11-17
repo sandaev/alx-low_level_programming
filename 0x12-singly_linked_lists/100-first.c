384ab06a279ddcae0b715d541cbec1e434145c5e
Sanni <sandae20v@gmail.com>
2023-11-17 05:18:30 +0100

    100 initial code

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * start_func - Executes before main executes
+ *
+ * Description: This fucntion prints a string before the main
+ * excutes
+ *
+ * Return: Nothing
+ */
+void start_func(void)
+{
+	char *str = "You're beat! and yest, you must allow,";
+	char *str2 = "\mI bore my house upon my back!";
+
+	printf("%s%s\n", str, str2);
+}
